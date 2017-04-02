ecaf8fb12493c85bb000189a0d0ce6e8919cc317
hgarcia <hernan@dynamicprogrammer.com>
2017-04-01 23:12:43 -0400

    [HG] Clone and 1/2 presenter mode....

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -2,24 +2,51 @@
 
 
 const DynamicProg = {
+  setPresenterMode() {
+    DynamicProg.initPresenterMode();
+  },
+  initPresenterMode() {
+    document
+      .querySelectorAll("blockquote")
+      .forEach((node) => {
+        node.className = "notes";
+      });
+  },
+  receiveTabChange(event) {
+    if (event.key === "prezo-navigate") {
+      const file = DynamicProg.file(window.location.pathname),
+        newFile = DynamicProg.file(event.newValue);
+      DynamicProg.navigate(DynamicProg.location(file, newFile));
+    }
+  },
   newFile(file, change) {
     const current = file.replace("slides-", "").replace(".html", "");
     return `slides-${parseInt(current, 10) + change}.html`;
   },
-  file() {
-    const path = window.location.pathname.split("/");
+  file(locationPath) {
+    const path = locationPath.split("/");
     return path[path.length - 1];
   },
+  navigate(location) {
+    window.location = location;
+    localStorage.setItem("prezo-navigate", location);
+  },
+  hash() {
+    return window.location.hash;
+  },
+  location(file, newFile) {
+    return `${window.location.pathname.replace(file, newFile)}`;
+  },
   goAhead() {
-    const file = DynamicProg.file(),
+    const file = DynamicProg.file(window.location.pathname),
       newFile = DynamicProg.newFile(file, 1);
-    window.location = window.location.pathname.replace(file, newFile);
+    DynamicProg.navigate(DynamicProg.location(file, newFile));
   },
   goBack() {
-    const file = DynamicProg.file(),
+    const file = DynamicProg.file(window.location.pathname),
       newFile = DynamicProg.newFile(file, -1);
     if (newFile !== "slides--1.html") {
-      window.location = window.location.pathname.replace(file, newFile);
+      DynamicProg.navigate(DynamicProg.location(file, newFile));
     }
   },
   codeRunner(id) {
@@ -38,25 +65,40 @@ const DynamicProg = {
   captureArrow(e) {
     const event = e || window.event;
     if (event) {
-      switch  (event.keyCode) {
-        case 8:
-          DynamicProg.goBack();
-          break;
-        case 13:
-          DynamicProg.goAhead();
-          break;
-        case 37:
-          DynamicProg.goBack();
-          break;
-        case 39:
-          DynamicProg.goAhead();
-          break;
-        default:
-          //do nothing
-      }
+      DynamicProg.processKeyCode(event.keyCode);
+    }
+  },
+  cloneWindow() {
+    window.open(window.location);
+  },
+  processKeyCode(keyCode) {
+    switch  (keyCode) {
+      case 8:
+        DynamicProg.goBack();
+        break;
+      case 13:
+        DynamicProg.goAhead();
+        break;
+      case 37:
+        DynamicProg.goBack();
+        break;
+      case 39:
+        DynamicProg.goAhead();
+        break;
+      case 67:
+        DynamicProg.cloneWindow();
+        break;
+       case 80:
+        DynamicProg.setPresenterMode();
+        break;
+      default:
+        // console.log(keyCode);
+        //do nothing
     }
   }
 };
 
 document.onkeydown = DynamicProg.captureArrow;
+window.addEventListener("storage", DynamicProg.receiveTabChange);
+// DynamicProg.initPresenterMode();
 DynamicProg.displayConsole();
